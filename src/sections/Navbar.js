e40ff13e7c92e000711a4c065cefce26b252759b
Lotachi <lotaoko2@gmail.com>
2023-09-12 04:23:12 +0100

    Your commit message here

--- src/sections/Navbar.js
+++ src/sections/Navbar.js
@@ -4,14 +4,14 @@ import Topbtn from '../images/topbtn.png'
 
 function Navbar() {
   return (
-    <div className='flex mt-4 justify-between burger flex-row items-center align-middle px-20'>
+    <div className='flex mt-4 justify-between burger flex-row items-center align-middle px-20 lg:hidden'>
 
       <img src={Pepehead} alt="" className='w-20' />
 
       <div className="nav w-[700px] border-black border bg-white text-black h-14 rounded-2xl flex items-center 
         align-middle p-4 
         ">
-        <div className='flex w-full text-sm   justify-between   border-2'>
+        <div className='flex w-full text-sm   justify-between    '>
           <ul>ABOUT</ul>
           <ul>HOW TO BUY</ul>
           <ul>TOKENOMICS</ul>
