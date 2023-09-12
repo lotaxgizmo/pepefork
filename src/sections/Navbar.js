8337d6c469d97441f3c05a83babdbf13b22b730d
Lotachi <lotaoko2@gmail.com>
2023-09-12 04:20:55 +0100

    Your commit message here

--- src/sections/Navbar.js
+++ src/sections/Navbar.js
@@ -1,21 +1,29 @@
 import React from 'react'
+import Pepehead from '../images/Pepehead.png'
+import Topbtn from '../images/topbtn.png'
 
 function Navbar() {
   return (
-    <div className='flex  justify-center burger'>
-        
-        <div className="nav w-[700px] border-black border bg-white text-black h-14 rounded-2xl flex items-center 
-        align-middle p-4 mt-4
-        ">
-            <div className='flex w-full text-sm   justify-between   border-2'>
-                <ul>ABOUT</ul>
-                <ul>HOW TO BUY</ul>
-                <ul>TOKENOMICS</ul>
-                <ul>ROADMAP</ul>
-                <ul>MEME GALLERY</ul>
-            </div>
-        </div>
+    <div className='flex mt-4 justify-between burger flex-row items-center align-middle px-20'>
 
+      <img src={Pepehead} alt="" className='w-20' />
+
+      <div className="nav w-[700px] border-black border bg-white text-black h-14 rounded-2xl flex items-center 
+        align-middle p-4 
+        ">
+        <div className='flex w-full text-sm   justify-between   border-2'>
+          <ul>ABOUT</ul>
+          <ul>HOW TO BUY</ul>
+          <ul>TOKENOMICS</ul>
+          <ul>ROADMAP</ul>
+          <ul>MEME GALLERY</ul>
+        </div>
+      </div>
+
+      <div>
+        <img src={Topbtn} alt="" className='w-40' />
+
+      </div>
     </div>
   )
 }
